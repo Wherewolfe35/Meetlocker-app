21ba5a2f5fdc090efeb14a488e1d639b3a866854
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-09-11 17:03:04 -0500

    adds edit and delete icons for specific users

--- src/components/CampLog/CampLog.js
+++ src/components/CampLog/CampLog.js
@@ -4,11 +4,8 @@ import { connect } from "react-redux";
 import "./CampLog.css";
 import { TextField, Grid, Button } from "@material-ui/core";
 import DateFnsUtils from '@date-io/date-fns';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
-import AddCommentTwoToneIcon from '@material-ui/icons/AddCommentTwoTone';
+import { MuiPickersUtilsProvider, KeyboardDatePicker, } from '@material-ui/pickers';
+import { AddCommentTwoTone, Edit as EditIcon, DeleteForeverOutlined } from '@material-ui/icons';
 
 const CampLog = (props) => {
   const [selectedDate, setSelectedDate] = React.useState(new Date('2019-09-11T21:11:54'));
@@ -17,38 +14,38 @@ const CampLog = (props) => {
 
   React.useEffect(() => {
     props.dispatch({ type: 'GET_LOG' });
-  }, []);
+  }, []); //upon page load, get logs from database
 
   function handleDateChange(date, event) {
     setSelectedDate(date);
     props.dispatch({ type: 'LOG_DATE', payload: event });
-  }
+  } // sets Date for new log
 
   function handleNewPost(event) {
     props.dispatch({ type: 'CURRENT_LOG', payload: event.target.value });
-  }
+  } //keeps track of text within log text field
 
   function handleSubmit() {
     props.dispatch({ type: 'ADD_LOG', payload: props.log });
-  }
+  } //sends message to saga to POST new log
 
   function handleComment(id) {
     setComment(true);
     setCommentId(id);
-  }
+  } //opens up comment text field
 
   function commentText(event) {
     props.dispatch({ type: 'CURRENT_COMMENT', payload: { id: commentId, text: event.target.value } })
-  }
+  } //keeps track of text in comment text field
 
   function submitComment() {
-    props.dispatch({ type: 'ADD_COMMENT', payload: props.comment});
+    props.dispatch({ type: 'ADD_COMMENT', payload: props.comment });
     setComment(false);
-  }
+  }//sends message to saga to POST new comment and closes text field
 
   function displayComments(id) {
-    props.dispatch({type: 'GET_COMMENTS', payload: id});
-  }
+    props.dispatch({ type: 'GET_COMMENTS', payload: id });
+  } //shows comment list for selected log
 
   return (
     <>
@@ -83,30 +80,44 @@ const CampLog = (props) => {
       <ul className="logList">
         {props.logList && props.logList.map(log =>
           <div key={log.id}>
-            <p>{log.post}<span className="logNameDate"> ~{log.name} {log.date}</span></p>
-            {comment && commentId === log.id && 
-            <div><TextField
-              id="filled-multiline-flexible"
-              label="Comment"
-              multiline
-              fullWidth
-              rowsMax="2"
-              onChange={commentText}
-              margin="dense"
-              variant="filled"
-              value={props.comment.text}
-            />
-              <AddCommentTwoToneIcon fontsize="small" onClick={submitComment} />
-            </div>
-          }
+            <p>{log.post}
+              <span className="logNameDate"> ~{log.name} {log.date}</span>
+              {log.user_id === props.userId && <span>
+                <EditIcon size="small" /> &nbsp;
+                <DeleteForeverOutlined size="small" />
+              </span>
+              }
+            </p>
+            {comment && commentId === log.id &&
+              <div><TextField
+                id="filled-multiline-flexible"
+                label="Comment"
+                multiline
+                fullWidth
+                rowsMax="2"
+                onChange={commentText}
+                margin="dense"
+                variant="filled"
+                value={props.comment.text}
+              />
+                <AddCommentTwoTone fontsize="small" onClick={submitComment} />
+              </div>
+            }
             <Button size="small" variant="outlined" onClick={() => handleComment(log.id)}>Comment</Button>
-            <span><Button size="small" onClick={()=>displayComments(log.id)}><u>See Comments</u></Button></span>
-            {props.commentList && props.commentList.map((comment) =>{ 
-              if(comment.logs_id === log.id){
-                return(
-            <div key={comment.id} className="commentList">
-              <p>{comment.comments_post} <span> ~{comment.name}</span></p>
-            </div>)}})
+            <span><Button size="small" onClick={() => displayComments(log.id)}><u>See Comments</u></Button></span>
+            {props.commentList && props.commentList.map((comment) => {
+              if (comment.logs_id === log.id) {
+                return (
+                  <div key={comment.id} className="commentList">
+                    <p>{comment.comments_post} <span> ~{comment.name} 
+                    {comment.users_id === props.userId &&
+                        <DeleteForeverOutlined size="small" />
+                    }
+                    </span>
+                    </p>
+                  </div>)
+              }
+            })
             }
           </div>
         )}
@@ -121,6 +132,7 @@ const mapStateToProps = (state) => ({
   comment: state.log.currentComment,
   log: state.log.currentLog,
   commentList: state.log.commentList,
+  userId: state.user.id
 });
 
 export default connect(mapStateToProps)(CampLog);
